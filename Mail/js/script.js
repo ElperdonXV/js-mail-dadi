7b47f35a99afa207576e6a99fa946af54b5954e3
ElperdonXV <davidemartinografico@gmail.com>
2021-11-24 15:22:09 +0100

    work on mail

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,3 +1,12 @@
 //Chiedi all’utente la sua email,
 //controlla che sia nella lista di chi può accedere,
 //stampa un messaggio appropriato sull’esito del controllo.
+
+const mail = prompt('Inserisci la tua email');
+const registeredMail = ["ciao@italia.it" , "rosso@rosso.it" , "carmelo@melo.it"];
+for (let index=0; index < 3; index++ ){
+    if(mail == registeredMail[index]){
+        console.log('Login effettuato corretamente');
+    }
+    else console.log('Email non registrata');
+}
