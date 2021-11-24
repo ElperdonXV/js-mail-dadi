8505c063f3aa67020470daad81c07aa50ca0b390
ElperdonXV <davidemartinografico@gmail.com>
2021-11-24 15:50:16 +0100

    work on dadi

--- Giocodeidadi/js/script.js
+++ Giocodeidadi/js/script.js
@@ -1,2 +1,9 @@
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const random = [];
+for (i=0; i<2; i++){
+    random.push(Math.floor(Math.random() * (6 - 1 + 1)));
+}
+
+console.log(random);
